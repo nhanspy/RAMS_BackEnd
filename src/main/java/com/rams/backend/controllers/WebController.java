22b66c207ccf94410209cca6c3b4479a34a4d260
nhanspy <nhan0095@gmail.com>
2021-04-09 19:24:16 +0700

    demo tu loda

--- src/main/java/com/rams/backend/controllers/WebController.java
+++ src/main/java/com/rams/backend/controllers/WebController.java
@@ -6,14 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
-//    @GetMapping(value = {"/", "/home"})
-//    public String homepage() {
-//        return "home";
-//    }
-//
-//    @GetMapping("/hello")
-//    public String hello() {
-//        return "hello";
-//    }
+    @GetMapping(value = {"/", "/home"})
+    public String homepage() {
+        return "home";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
 
 }
--- src/main/java/com/rams/backend/payload/LoginResponse.java
+++ src/main/java/com/rams/backend/payload/LoginResponse.java
@@ -18,4 +18,28 @@ public class LoginResponse {
     public LoginResponse(String accessToken) {
         this.accessToken = accessToken;
     }
+
+    @Override
+    public String toString() {
+        return "LoginResponse{" +
+                "accessToken='" + accessToken + '\'' +
+                ", tokenType='" + tokenType + '\'' +
+                '}';
+    }
+
+    //    public String getAccessToken() {
+//        return accessToken;
+//    }
+//
+//    public void setAccessToken(String accessToken) {
+//        this.accessToken = accessToken;
+//    }
+//
+//    public String getTokenType() {
+//        return tokenType;
+//    }
+//
+//    public void setTokenType(String tokenType) {
+//        this.tokenType = tokenType;
+//    }
 }
